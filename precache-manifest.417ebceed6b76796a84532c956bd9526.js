0cf82aadc0666744cab352ee3554fefd0464e553
Hanzhi Teng <tenghanzhi@outlook.com>
2024-09-29 16:16:05 -0700

    Updates

---
+++ precache-manifest.417ebceed6b76796a84532c956bd9526.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3246c6d8b87f11f9cceb10438e24597",
+    "url": "/customer-list-react/index.html"
+  },
+  {
+    "revision": "024d91468ac7fec420d4",
+    "url": "/customer-list-react/static/css/4.17f8cf15.chunk.css"
+  },
+  {
+    "revision": "ee69fcc59e688cacbea9",
+    "url": "/customer-list-react/static/css/main.705b9945.chunk.css"
+  },
+  {
+    "revision": "024d91468ac7fec420d4",
+    "url": "/customer-list-react/static/js/4.2b5f1f69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/customer-list-react/static/js/4.2b5f1f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea9f98b62714f556ab84",
+    "url": "/customer-list-react/static/js/5.6c036d79.chunk.js"
+  },
+  {
+    "revision": "90b6b156a5f62810eddc9189ae0e43d4",
+    "url": "/customer-list-react/static/js/5.6c036d79.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ff8304db870c9faf2e5",
+    "url": "/customer-list-react/static/js/6.3b219ef2.chunk.js"
+  },
+  {
+    "revision": "ee69fcc59e688cacbea9",
+    "url": "/customer-list-react/static/js/main.c126773b.chunk.js"
+  },
+  {
+    "revision": "78a2adb8cb663db0e2a1",
+    "url": "/customer-list-react/static/js/runtime-main.32c7278e.js"
+  },
+  {
+    "revision": "4fd15c4246d9bf61e607",
+    "url": "/customer-list-react/static/js/stencil-h8k-logo_2-entry-js.5223fb50.chunk.js"
+  },
+  {
+    "revision": "a9a021a77dd9743d30f3",
+    "url": "/customer-list-react/static/js/stencil-polyfills-css-shim.54423c61.chunk.js"
+  }
+]);
